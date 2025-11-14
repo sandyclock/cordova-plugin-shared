085661bc462dfe502db1776e4cc2888c39b7df80
Li Tan <tanli.stonybrook@gmail.com>
2025-11-13 19:11:55 -0800

    update plugin for use with capacitor

--- src/android/com/missiveapp/openwith/IntentActivity.java
+++ src/android/com/missiveapp/openwith/IntentActivity.java
@@ -1,186 +1,79 @@
 package com.missiveapp.openwith;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.ClipData;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+import androidx.annotation.RequiresApi;
 import android.util.Log;
-import android.util.SparseArray;
-
-import com.adobe.phonegap.push.FCMService;
-import com.adobe.phonegap.push.PushPlugin;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import static java.lang.System.*;
-
+/**
+ * Activity that intercepts share intents and processes them before launching MainActivity.
+ * 
+ * This activity is registered in AndroidManifest to receive ACTION_SEND and ACTION_SEND_MULTIPLE intents.
+ * It processes the intent (extracts files, scans QR codes, fetches HTML content) and then
+ * launches MainActivity with the processed data in JSON format.
+ */
 public class IntentActivity extends Activity {
   private static String LOG_TAG = "Shared_IntentActivity";
 
-  /*
-   * this activity will be started if the user touches a notification that we own.
-   * We send it's data off to the push plugin for processing.
-   * If needed, we boot up the main activity to kickstart the application.
-   * @see android.app.Activity#onCreate(android.os.Bundle)
+  /**
+   * Interface for launching MainActivity with processed intent data.
    */
+  public interface StartActivityFun {
+    void start(org.json.JSONObject extraObj);
+  }
+
   @RequiresApi(api = Build.VERSION_CODES.N)
   @Override
   public void onCreate(Bundle savedInstanceState) {
-
-    Intent intent = getIntent();
-
     super.onCreate(savedInstanceState);
-    Log.v(LOG_TAG, "onCreate");
+    Log.v(LOG_TAG, "onCreate - Processing share intent");
 
-    String callback = getIntent().getExtras().getString("callback");
-    Log.d(LOG_TAG, "callback = " + callback);
-    boolean foreground = getIntent().getExtras().getBoolean("foreground", true);
-//    boolean startOnBackground = getIntent().getExtras().getBoolean(START_IN_BACKGROUND, false);
-//    boolean dismissed = getIntent().getExtras().getBoolean(DISMISSED, false);
-//    Log.d(LOG_TAG, "dismissed = " + dismissed);
+    // Process the intent and launch MainActivity
+    forceMainActivityReload();
 
-//    if(!startOnBackground){
-//      NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-//      notificationManager.cancel(FCMService.getAppName(this), notId);
-//    }
-//
-    boolean isPushPluginActive = PushPlugin.isActive();
-//    boolean inline = processPushBundle(isPushPluginActive, intent);
-
-//    if(inline && android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.N && !startOnBackground){
-//      foreground = true;
-//    }
-//
-//    Log.d(LOG_TAG, "bringToForeground = " + foreground);
-
+    // Finish this activity immediately (it's just an intermediary)
     finish();
-
-
-//    if (isPushPluginActive) {
-    forceMainActivityReload(false);
-//      if (intent.getAction().equals(Intent.ACTION_SEND)){
-//        //this is an sending activity. We need to process this.
-////        ContentResolver resolver = this.getApplicationContext().getContentResolver();
-//        try {
-//          JSONObject obj = Serializer.toJSONObject(this, intent);
-////          Log.d(LOG_TAG, "obj:");
-////          Log.d(LOG_TAG, obj.toString());
-//
-//        } catch (JSONException e) {
-//          e.printStackTrace();
-//        }
-////        startActivity(intent);
-//      }
-
-
-//    };
-//      if (!isPushPluginActive && foreground && inline) {
-//        Log.d(LOG_TAG, "forceMainActivityReload");
-//        forceMainActivityReload(false);
-//      } else if(startOnBackground) {
-//        Log.d(LOG_TAG, "startOnBackgroundTrue");
-//        forceMainActivityReload(true);
-//      } else {
-//        Log.d(LOG_TAG, "don't want main activity");
-//      }
-  }
-
-//  /**
-//   * Takes the pushBundle extras from the intent,
-//   * and sends it through to the PushPlugin for processing.
-//   */
-//  private boolean processPushBundle(boolean isPushPluginActive, Intent intent) {
-//    Bundle extras = getIntent().getExtras();
-//    Bundle remoteInput = null;
-//
-//    if (extras != null) {
-//      Bundle originalExtras = extras.getBundle(PUSH_BUNDLE);
-//
-//      originalExtras.putBoolean(FOREGROUND, false);
-//      originalExtras.putBoolean(COLDSTART, !isPushPluginActive);
-//      originalExtras.putBoolean(DISMISSED, extras.getBoolean(DISMISSED));
-//      originalExtras.putString(ACTION_CALLBACK, extras.getString(CALLBACK));
-//      originalExtras.remove(NO_CACHE);
-//
-//      remoteInput = RemoteInput.getResultsFromIntent(intent);
-//      if (remoteInput != null) {
-//        String inputString = remoteInput.getCharSequence(INLINE_REPLY).toString();
-//        Log.d(LOG_TAG, "response: " + inputString);
-//        originalExtras.putString(INLINE_REPLY, inputString);
-//      }
-//
-//      PushPlugin.sendExtras(originalExtras);
-//    }
-//    return remoteInput == null;
-//  }
-
-  public interface StartActivityFun {
-    void start(JSONObject extraObj);
   }
 
   /**
-   * Forces the main activity to re-launch if it's unloaded.
+   * Processes the share intent and launches MainActivity with the processed data.
+   * 
+   * The intent is processed by Serializer.populateAndSendIntent() which:
+   * - Extracts files/images from the intent
+   * - Scans for QR codes in images
+   * - Fetches HTML content for URLs
+   * - Converts everything to JSON format
    */
   @RequiresApi(api = Build.VERSION_CODES.N)
-  private void forceMainActivityReload(boolean startOnBackground) {
-
-//    Intent originalIntent = getIntent();
+  private void forceMainActivityReload() {
     Bundle extras = getIntent().getExtras();
-    if (extras != null) {
+    if (extras == null) {
+      Log.w(LOG_TAG, "No extras found in intent");
+      return;
+    }
 
-//      launchIntent.putExtras(extras);
-//       ClipData data = getIntent().getClipData();
-//       launchIntent.setClipData(data);
+    StartActivityFun sendIntent = (org.json.JSONObject json) -> {
+      PackageManager pm = getPackageManager();
+      Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
 
-//       String type = getIntent().getType();
-//       if (type!=null) {
-//         launchIntent.putExtra("internalType", type);
-////         launchIntent.setType(type);
-//       }
-//      JSONObject obj = null;
-      StartActivityFun sendIntent = (JSONObject json) -> {
-        PackageManager pm = getPackageManager();
-        Intent launchIntent = pm.getLaunchIntentForPackage(getApplicationContext().getPackageName());
-
-        if (json != null) {
-          launchIntent.putExtra("json", json.toString());
-        }
-
-        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-//        launchIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
-
-        launchIntent.putExtra("cdvStartInBackground", false);
-        this.startActivity(launchIntent);
-      };
-
-      try {
-//        obj = Serializer.toJSONObject(getApplicationContext().getContentResolver(), getIntent());
-        Serializer.populateAndSendIntent(this, getIntent(), sendIntent);
-//        sendIntent.start(obj);
-
-//        launchIntent.putExtra("json", obj.toString());
-      } catch (JSONException e) {
-        e.printStackTrace();
+      if (json != null) {
+        launchIntent.putExtra("json", json.toString());
       }
 
-    }
-  }
+      launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      launchIntent.putExtra("cdvStartInBackground", false);
+      this.startActivity(launchIntent);
+    };
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-//    final NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-//    notificationManager.cancelAll();
+    try {
+      Serializer.populateAndSendIntent(this, getIntent(), sendIntent);
+    } catch (JSONException e) {
+      Log.e(LOG_TAG, "Error processing intent: " + e.getMessage(), e);
+    }
   }
 }
